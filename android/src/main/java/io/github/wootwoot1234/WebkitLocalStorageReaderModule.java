c929827e6c5e0b040d6e1ec621cc5c93f7b0aa8e
Junior Oliveira <arojunior@gmail.com>
2018-12-03 19:24:26 -0200

    add support to localForage database

--- android/src/main/java/io/github/wootwoot1234/WebkitLocalStorageReaderModule.java
+++ android/src/main/java/io/github/wootwoot1234/WebkitLocalStorageReaderModule.java
@@ -30,16 +30,26 @@ public class WebkitLocalStorageReaderModule extends ReactContextBaseJavaModule {
         WritableMap kv = new WritableNativeMap();
         String dataDir = getReactApplicationContext().getApplicationInfo().dataDir;
         File localstorage = new File(dataDir + "/app_webview/Local Storage/file__0.localstorage");
+        File localforage = new File(dataDir + "/databases/localforage");
 
-        if (!localstorage.exists()) {
+        if (!localstorage.exists() && !localforage.exists()) {
             promise.resolve(kv);
             return;
         }
 
+        if (localstorage.exists()) {
+            readLocalStorage(localstorage, kv, promise);
+        } else {
+            readLocalForage(localforage, kv, promise);
+        }
+
+    }
+
+    private void readLocalStorage(File appDatabase, WritableMap kv, Promise promise) {
         Cursor cursor = null;
         SQLiteDatabase db = null;
         try {
-            File dbfile = getReactApplicationContext().getDatabasePath(localstorage.getPath());
+            File dbfile = getReactApplicationContext().getDatabasePath(appDatabase.getPath());
             dbfile.setWritable(true);
             db = SQLiteDatabase.openDatabase(dbfile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READWRITE);
 
@@ -52,6 +62,44 @@ public class WebkitLocalStorageReaderModule extends ReactContextBaseJavaModule {
                 String value = new String(itemByteArray, Charset.forName("UTF-16LE"));
 
                 kv.putString(key, value);
+                cursor.moveToNext();
+            }
+            promise.resolve(kv);
+        } catch (Exception e) {
+            e.printStackTrace();
+            promise.resolve(kv);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
+                db.close();
+            }
+        }
+    }
+
+    private void readLocalForage(File appDatabase, WritableMap kv, Promise promise) {
+        Cursor cursor = null;
+        SQLiteDatabase db = null;
+        try {
+            File dbfile = getReactApplicationContext().getDatabasePath(appDatabase.getPath());
+            dbfile.setWritable(true);
+            db = SQLiteDatabase.openDatabase(dbfile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READWRITE);
+
+            String sql = "SELECT key,value FROM keyvaluepairs";
+            cursor = db.rawQuery(sql, null);
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+
+                String key = cursor.getString(0);
+                String value = cursor.getString(1);
+
+                kv.putString(key, value);
+
+                if (cursor.getString(0).equals("mobileConfigurations")) {
+                    break;
+                }
+
                 cursor.moveToNext();
             }
             promise.resolve(kv);
